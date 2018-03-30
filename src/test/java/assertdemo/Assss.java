decd63f76eafe02a3a9e97158d26a46aac8e4554
Ganesh <Ganesh@DESKTOP-IKUQHQG>
2018-03-30 13:51:53 +0530

    test case updated

---
+++ src/test/java/assertdemo/Assss.java
@@ -0,0 +1,21 @@
+package assertdemo;
+
+import org.testng.annotations.Test;
+
+public class Assss {
+
+	@Test
+	public void ssn() {
+		
+		/*softassert assertion = new softassert();
+		
+		assertion.assertequals(12,13);
+		
+		assertion.assertall();
+		*/
+		
+		
+		
+		
+	}
+}
